66871a812ea59e0f094c2d6896fdaac80962d8f2
Рогатнев Сергей <s.rogatnev@skbkontur.ru>
2018-04-07 13:39:50 +0700

    - units moves, world generator

---
+++ Warxels/GameLogic/World.cs
@@ -0,0 +1,25 @@
+﻿namespace GameLogic
+{
+    internal sealed class World : IWorld
+    {
+        private readonly Army _army;
+
+        public World(int length, int width, Army army)
+        {
+            _army = army;
+            Length = length;
+            Width = width;
+        }
+
+        public int Length { get; }
+
+        public int Width { get; }
+
+        public IArmy Army => _army;
+
+        public void MoveUnit(UnitBase unit, int y, int x)
+        {
+            _army.Move(unit, y, x);
+        }
+    }
+}
