2aaeb5db47c7067f4852a138a514f6e4d8ce390f
SIlov Yan <ya.yyaann@yandex.ru>
2018-04-08 12:02:02 +0700

    Добавил окно результатов

--- Warxels/DevUiAndroidV2/BattleView.cs
+++ Warxels/DevUiAndroidV2/BattleView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Android.Content;
 using Android.Graphics;
 using Android.Views;
@@ -8,14 +9,17 @@ namespace DevUiAndroidV2
 {
     class BattleView : View
     {
-        public bool EndGame { get; set; } = false;
+        public bool EndGame => _lastMoveAgo > 50;
         public int Delay { get; } = 10;
-        public bool Paused { get; set; } = false;
         public IWorld World { get; }
         private const int SIZE = MyView.SIZE;
+        private int _lastMoveAgo=0;
         private int step;
         private Paint marshPaint = new Paint { Color = new Color(0, 0, 255, 64) };
         private Paint projectilePen = new Paint { Color = Color.Brown };
+
+        public int CountBlueDead { get; private set; } = 0;
+        public int CountRedDead { get; private set; } = 0;
 
 
         private Paint[] TeamAPens = new Paint[] {
@@ -118,8 +122,17 @@ namespace DevUiAndroidV2
         {
             step = Right / SIZE;
 
-            if (!Paused)
-                World.DoTick();
+            if (!EndGame)
+            {
+
+                var result = World.DoTick();
+                if (result.WasMoves)
+                    _lastMoveAgo = 0;
+                else
+                    _lastMoveAgo++;
+                CountBlueDead += result.DeadUnits.Count(c => c.Team == Team.Blue);
+                CountRedDead += result.DeadUnits.Count(c => c.Team == Team.Red);
+            }
             canvas.DrawARGB(255, 255, 255, 255);
 
             DrawTerrain(canvas);
