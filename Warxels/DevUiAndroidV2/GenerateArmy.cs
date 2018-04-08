2cf701861abf871701c10032e5e5296e5815cf29
Рогатнев Сергей <s.rogatnev@skbkontur.ru>
2018-04-08 09:22:52 +0700

    - добавил генерацию врагов

--- Warxels/DevUiAndroidV2/GenerateArmy.cs
+++ Warxels/DevUiAndroidV2/GenerateArmy.cs
@@ -20,16 +20,16 @@ namespace DevUiAndroidV2
             if (CheckSquad(squad))
             {
                 _squads.Add(squad);
-                return true; 
-            }                           
+                return true;
+            }
             return false;
         }
 
         public bool CheckSquad(ISquad squad)
         {
-            var inWorldSpace = squad.MinX >= 0 && squad.MaxX < MyView.SIZE 
-                               && squad.MinY >= MyView.SIZE/2 && squad.MaxY < MyView.SIZE;
-            return inWorldSpace && (_squads.Count == 0 || _squads.All(val => squad==val 
+            var inWorldSpace = squad.MinX >= 0 && squad.MaxX < MyView.SIZE
+                               && squad.MinY >= MyView.SIZE / 2 && squad.MaxY < MyView.SIZE;
+            return inWorldSpace && (_squads.Count == 0 || _squads.All(val => squad == val
                                     || val.MaxX < squad.MinX || val.MinX > squad.MaxX
                                      || val.MinY > squad.MaxY || val.MaxY < squad.MinY));
         }
@@ -40,10 +40,28 @@ namespace DevUiAndroidV2
             foreach (var s in _squads)
             {
                 for (var x = s.MinX; x < s.MaxX; x++)
-                for (var y = s.MinY; y < s.MaxY; y++)
-                    generator.CreateUnit(s.Type, s.Team, y+MyView.SIZE/3, x);
+                {
+                    for (var y = s.MinY; y < s.MaxY; y++)
+                    {
+                        generator.CreateUnit(s.Type, s.Team, y + MyView.SIZE / 3, x);
+                    }
+                }
             }
+
+            GenerateEnemies(generator);
+
             return generator.GetWorld();
+        }
+
+        public void GenerateEnemies(WorldsGenerator generator)
+        {
+            generator.AddUnitSquare(Team.Blue, 10, 10, 10, 10, UnitType.HorseMan, 100);
+            generator.AddUnitSquare(Team.Blue, 10, 80, 10, 10, UnitType.HorseMan, 100);
+
+            generator.AddUnitSquare(Team.Blue, 10, 30, 40, 10, UnitType.Archer, 400);
+
+            generator.AddUnitSquare(Team.Blue, 30, 20, 20, 10, UnitType.SwordsMan, 200);
+            generator.AddUnitSquare(Team.Blue, 30, 60, 20, 10, UnitType.SwordsMan, 200);
         }
     }
 }
